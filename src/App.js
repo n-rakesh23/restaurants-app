476ca25cacdb34f9f6f55b76a265b885fcdcdc12
RakeshNayak <n.rakesh0023@gmail.com>
2025-11-13 15:43:22 +0530

    small changes in index.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import "./restaurantBody.css";
-import "./footer.css";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 const Navbar= ()=>{
     const navItems = [
